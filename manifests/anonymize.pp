5bd5b658450c4ff636e9af4cbd5c64519af1e1cc
mh <mh@immerda.ch>
2013-06-12 00:44:13 +0200

    and edit the file in place

--- manifests/anonymize.pp
+++ manifests/anonymize.pp
@@ -1,6 +1,6 @@
 # anonymize ips in gitolite logs
 class gitolite::anonymize {
-  exec{'sed \'s/\$ENV{SSH_CONNECTION} || //\' /usr/share/gitolite3/gitolite-shell':
+  exec{'sed -i \'s/\$ENV{SSH_CONNECTION} || //\' /usr/share/gitolite3/gitolite-shell':
     onlyif  => 'grep -q \'$ip = $ENV{SSH_CONNECTION} ||\' /usr/share/gitolite3/gitolite-shell',
     require => Package['gitolite'],
   }
