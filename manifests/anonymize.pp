d19156c5c29c5d1d5bfaba9377c92c91a8f44b25
mh <mh@immerda.ch>
2013-06-12 00:33:06 +0200

    fix typo

--- manifests/anonymize.pp
+++ manifests/anonymize.pp
@@ -1,7 +1,7 @@
 # anonymize ips in gitolite logs
 class gitolite::anonymize {
   exec{'sed \'s/\$ENV{SSH_CONNECTION} || //\' /usr/share/gitolite3/gitolite-shell':
-    unless  => 'grep -q \'$ip = $ENV{SSH_CONNECTION} ||\' /usr/share/gitolite3/gitolite-shell'
+    unless  => 'grep -q \'$ip = $ENV{SSH_CONNECTION} ||\' /usr/share/gitolite3/gitolite-shell',
     require => Package['gitolite'],
   }
 }
