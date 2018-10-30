646eb0fdf9824074822e5a48202daae64cbcbeab
mh <mh@immerda.ch>
2018-10-31 00:30:14 +0100

    this needs to change

--- manifests/daemon/selinux.pp
+++ manifests/daemon/selinux.pp
@@ -2,13 +2,18 @@
 class gitolite::daemon::selinux {
   include ::gitolite
   include ::gitolite::base
+  if versioncmp($facts['os']['release']['major'],'7') >= 0 {
+    $te_source = 'puppet:///modules/gitolite/selinux/daemon/git_daemon_gitolite.te'
+  } else {
+    $te_source = 'puppet:///modules/gitolite/selinux/daemon/git_daemon_gitolite.te.CentOS.6'
+  }
   selboolean{'git_system_enable_homedirs':
     value       => 'on',
     persistent  => true,
     require     => Package['git-daemon'],
   } -> selinux::policy{
     'git_daemon_gitolite':
-      te_source => 'puppet:///modules/gitolite/selinux/daemon/git_daemon_gitolite.te',
+      te_source => $te_source,
   } -> selinux::fcontext{'/home/[^/]+/repositories(/.*)?':
     setype => $gitolite::base::setype,
   } -> Service<| title == 'xinetd' or title == 'git.socket' |>
